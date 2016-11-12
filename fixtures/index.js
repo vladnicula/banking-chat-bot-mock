26631036a80e23ba61f8731a94662c7b32c922cb
Raul Rene Lepsa <raul.lepsa@gmail.com>
2016-11-12 23:37:03 +0200

    Setting user balance and savings balance

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,11 +1,11 @@
 const userService = require('../services/user-service');
 
 const USERS = [
-    {name: 'Raul', chatId: '981647388611508', currentAmmount: 1250.18},
-    {name: 'Vlad', chatId: '1221584201246326', currentAmmount: 322.53},
-    {name: 'Bogdan', chatId: '1325850250780262', currentAmmount: 320.00},
-    {name: 'Horia', chatId: '1203276786414242', currentAmmount: 500},
-    {name: 'Vivianne', chatId: '1130662566983525', currentAmmount: 755}
+    {name: 'Raul', chatId: '981647388611508', balance: 1250.18, balanceSavings: 3000},
+    {name: 'Vlad', chatId: '1221584201246326', balance: 322.53, balanceSavings: 3000},
+    {name: 'Bogdan', chatId: '1325850250780262', balance: 320.00, balanceSavings: 3000},
+    {name: 'Horia', chatId: '1203276786414242', balance: 500, balanceSavings: 3000},
+    {name: 'Vivianne', chatId: '1130662566983525', balance: 755, balanceSavings: 3000}
 ];
 
 const USER_IDS = USERS.map(userDetails => {
