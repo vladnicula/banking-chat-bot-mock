a0c0519599bdcfcbd6d46231257af88ee7fca990
Raul Rene Lepsa <raul.lepsa@gmail.com>
2016-11-12 12:24:23 +0200

    Testing to see if coords are fetched properly

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
-const textrazor = require('./text-analysis'); 
+const textrazor = require('./text-analysis');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,13 +16,13 @@ app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
-	textrazor('Transfer 100 $ to Vlad Nicula', function (operation, money, people) {
-res.send(`
+    textrazor('Transfer 100 $ to Vlad Nicula', function (operation, money, people) {
+        res.send(`
 The operation to do is: ${operation}.
 Sum & currency: ${money[0].sum} ${money[0].currency}.
 People involved: ${people}
 `);
-	});
+    });
 });
 
 // for Facebook verification
@@ -31,19 +31,19 @@ app.get('/webhook/', function (req, res) {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong token');
-})
+});
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
+    let messageData = {text: text};
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:FB_PAGE_ACCESS_TOKEN},
+        qs: {access_token: FB_PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
-            recipient: {id:sender},
-            message: messageData,
+            recipient: {id: sender},
+            message: messageData
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending messages: ', error)
         } else if (response.body.error) {
@@ -53,27 +53,29 @@ function sendTextMessage(sender, text) {
 }
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
+        let event = req.body.entry[0].messaging[i];
+        let sender = event.sender.id;
         if (event.message && event.message.text) {
             let text = event.message.text;
             textrazor(text, function (operation, money, people) {
-            	const message = `
+                const message = `
 The operation to do is: ${operation}.
 Sum & currency: ${money[0].sum} ${money[0].currency}.
 People involved: ${people}
 `;
-            	sendTextMessage(sender, "Text received, echo: " + message.substring(0, 200))	
+                console.log(event.message);
+                const coordinates = event.message.attachments.payload.coordinates;
+                sendTextMessage(sender, "Text received, echo: " + coordinates);
             });
         }
     }
     res.sendStatus(200)
-})
+});
 
 // Spin up the server
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('running on port', app.get('port'))
 });
 
