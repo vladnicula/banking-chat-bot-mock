a7deb260b9b12342b6b2c8622904d1003c038a79
Vlad Nicula <vladnicula@gmail.com>
2016-11-12 17:25:03 +0200

    Logger for interceptor of payment sending flow

--- actions/request-money-send.js
+++ actions/request-money-send.js
@@ -2,11 +2,14 @@ const USER_CHAT_IDS = require('../data/user-chat-ids');
 
 const isTemporarySendRequest = (message) => {
 	const parts = message.split(' ');
+
+	console.log('isTemporarySendRequest', message, parts.lenght, USER_CHAT_IDS[parts[1]]);
+
 	if ( parts.lenght !== 2 ) {
 		return false;
 	}
 
-	const name = parts[0];
+	const name = parts[1];
 	return USER_CHAT_IDS[name];
 };
 
