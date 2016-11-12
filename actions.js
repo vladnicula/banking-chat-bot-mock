d07bf7419ade918ba5e2a62be7171bd5f549cdca
Raul Rene Lepsa <raul.lepsa@gmail.com>
2016-11-12 17:00:52 +0200

    Trying out location response w/ WIT

--- actions.js
+++ actions.js
@@ -1,7 +1,9 @@
+'use strict';
+
 const actions = (fbMessage, sessions) => {
     return {
         send(request, response) {
-            sessionId = request.sessionId;
+            const sessionId = request.sessionId;
             text = response.text;
             // Our bot has something to say!
             // Let's retrieve the Facebook user whose session belongs to
@@ -26,18 +28,16 @@ const actions = (fbMessage, sessions) => {
                 return Promise.resolve()
             }
         },
+
         findATM(request) {
             const sessionId = request.sessionId;
             const recipientId = sessions[sessionId].fbid;
-            return fbMessage(recipientId, 'Test foarte specific din actions');
-            // return new Promise((resolve, reject) => {
-            // console.log('finding ATM');
-            //     return resolve({
-            //         location: 'iulius'
-            //     });
-            // });
+            return fbMessage(recipientId, {
+                "text": "Please share your location:",
+                "quick_replies": [{"content_type": "location"}]
+            });
         }
     }
-}
+};
 
 module.exports = actions;
