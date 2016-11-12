f85f6ce60facb9d07aba95edb9b4c008b331a074
bogdanpetru <bogdanpintican@gmail.com>
2016-11-12 20:57:22 +0200

    Add store for sessions

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
 const app = express();
 app.set('port', (process.env.PORT || 5000));
 const handleStaticActions = require('./static-actions');
+const store = require('./services/store')
 
 const Wit = require('node-wit').Wit;
 const log = require('node-wit').log;
@@ -44,7 +45,7 @@ const fbMessage = (id, message) => {
         });
 };
 
-const witActions = require('./wit-actions')(fbMessage, sessions);
+const witActions = require('./wit-actions')(fbMessage, store.getSessions());
 
 
 // Wit.ai parameters
@@ -56,22 +57,6 @@ const wit = new Wit({
     logger: new log.Logger(log.INFO)
 });
 
-const findOrCreateSession = (fbid) => {
-    let sessionId;
-    // Let's see if we already have a session for the user fbid
-    Object.keys(sessions).forEach(k => {
-        if (sessions[k].fbid === fbid) {
-            // Yep, got it!
-            sessionId = k;
-        }
-    });
-    if (!sessionId) {
-        // No session found for user fbid, let's create a new one
-        sessionId = new Date().toISOString();
-        sessions[sessionId] = {fbid: fbid, context: {}};
-    }
-    return sessionId;
-};
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
@@ -112,7 +97,7 @@ app.post('/webhook/', function (req, res) {
             return;
         }
 
-        const witSession = findOrCreateSession(sender);
+        const witSession = store.getSession(sender);
 
         console.log('JSON.stringify(event)', JSON.stringify(event));
 
---
+++ services/store.js
@@ -0,0 +1,28 @@
+const sessions = {}
+
+
+const findOrCreateSession = (fbid) => {
+    let sessionId;
+    // Let's see if we already have a session for the user fbid
+    Object.keys(sessions).forEach(k => {
+        if (sessions[k].fbid === fbid) {
+            // Yep, got it!
+            sessionId = k;
+        }
+    });
+    if (!sessionId) {
+        // No session found for user fbid, let's create a new one
+        sessionId = new Date().toISOString();
+        sessions[sessionId] = {fbid: fbid, context: {}};
+    }
+    return sessionId;
+};
+
+
+module.exports = {
+    getSenderId(sessionId) {
+      return sessions[sessionId].fbid
+    },
+    getSession: findOrCreateSession,
+    getSessions: () => sessions,
+}
