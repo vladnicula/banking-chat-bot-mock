c6ea689de5016a2ffab299ca9331179e6c1131f7
Raul Rene Lepsa <raul.lepsa@gmail.com>
2016-11-13 00:33:47 +0200

    Fixing getBalance method

--- actions/wit-actions.js
+++ actions/wit-actions.js
@@ -69,7 +69,7 @@ const actions = (fbMessage, sessions) => {
             const senderUser = userService.getUserByChatId(senderId);
             let text = senderUser.balance;
 
-            if (entities && entities.ownAccount[0] && entities.ownAccount[0].value === "savings") {
+            if (entities && entities.ownAccount && entities.ownAccount[0].value === "savings") {
                 text = senderUser.balanceSavings;
             }
 
