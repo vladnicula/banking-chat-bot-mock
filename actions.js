62b6a263586d69ef8a5841e05c663b1a10e8aa1f
ancashoria <horia.ancas@gmail.com>
2016-11-12 16:38:12 +0200

    First wit integration

---
+++ actions.js
@@ -0,0 +1,43 @@
+const actions = (fbMessage, sessions) => {
+    return {
+        send(request, response) {
+            sessionId = request.sessionId;
+            text = response.text;
+            // Our bot has something to say!
+            // Let's retrieve the Facebook user whose session belongs to
+            const recipientId = sessions[sessionId].fbid;
+            if (recipientId) {
+                // Yay, we found our recipient!
+                // Let's forward our bot response to her.
+                // We return a promise to let our bot know when we're done sending
+                return fbMessage(recipientId, text)
+                    .then(() => null)
+                    .catch((err) => {
+                        console.error(
+                            'Oops! An error occurred while forwarding the response to',
+                            recipientId,
+                            ':',
+                            err.stack || err
+                        );
+                    });
+            } else {
+                console.error('Oops! Couldn\'t find user for session:', sessionId);
+                // Giving the wheel back to our bot
+                return Promise.resolve()
+            }
+        },
+        findATM(request) {
+            const sessionId = request.sessionId;
+            const recipientId = sessions[sessionId].fbid;
+            return fbMessage(recipientId, 'Test foarte specific din actions');
+            // return new Promise((resolve, reject) => {
+            // console.log('finding ATM');
+            //     return resolve({
+            //         location: 'iulius'
+            //     });
+            // });
+        }
+    }
+}
+
+module.exports = actions;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const fetch = require('node-fetch');
 const app = express();
 const parser = require('./parse-request');
 
@@ -16,6 +17,62 @@ const USER_CHAT_IDS = {
 	'Bogdan': '1325850250780262',
 	'Horia': '1203276786414242',
 	'Vivianne': '1130662566983525'
+};
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+const sessions = {};
+
+const fbMessage = (id, text) => {
+  const body = JSON.stringify({
+    recipient: { id },
+    message: { text },
+  });
+  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
+    }
+    return json;
+  });
+};
+
+const Wit = require('node-wit').Wit;
+const log = require('node-wit').log;
+const actions = require('./actions')(fbMessage, sessions);
+// Webserver parameter
+const PORT = process.env.PORT || 8445;
+// Wit.ai parameters
+const WIT_TOKEN = '5JI7XC4RZL2LBDC47LDBU5X443ZFEFYX';
+// Setting up our bot
+const wit = new Wit({
+  accessToken: WIT_TOKEN,
+  actions,
+  logger: new log.Logger(log.INFO)
+});
+
+const findOrCreateSession = (fbid) => {
+    let sessionId;
+    // Let's see if we already have a session for the user fbid
+    Object.keys(sessions).forEach(k => {
+        if (sessions[k].fbid === fbid) {
+            // Yep, got it!
+            sessionId = k;
+        }
+    });
+    if (!sessionId) {
+        // No session found for user fbid, let's create a new one
+        sessionId = new Date().toISOString();
+        sessions[sessionId] = { fbid: fbid, context: {} };
+    }
+    return sessionId;
 };
 
 // Process application/x-www-form-urlencoded
@@ -60,6 +117,7 @@ app.post('/webhook/', function (req, res) {
     for (let i = 0; i < messaging_events.length; i++) {
         const event = req.body.entry[0].messaging[i];
         const sender = event.sender.id;
+        const witSession = findOrCreateSession(sender);
 
         const session = persistenceService.getSessionOfUserId(sender);
 
@@ -72,6 +130,31 @@ app.post('/webhook/', function (req, res) {
         if ( event.message ) {
             const response = parser(event);
             sendTextMessage(sender, response);
+
+            // Let's forward the message to the Wit.ai Bot Engine
+            // This will run all actions until our bot has nothing left to do
+            wit.runActions(
+                witSession, // the user's current session
+                event.message, // the user's message
+                sessions[witSession].context // the user's current session state
+            ).then((context) => {
+                // Our bot did everything it has to do.
+                // Now it's waiting for further messages to proceed.
+                console.log('Waiting for next user messages');
+
+                // Based on the session state, you might want to reset the session.
+                // This depends heavily on the business logic of your bot.
+                // Example:
+                // if (context['done']) {
+                //   delete sessions[witSession];
+                // }
+
+                // Updating the user's current session state
+                sessions[witSession].context = context;
+            })
+                .catch((err) => {
+                    console.error('Oops! Got an error from Wit: ', err.stack || err);
+                })
         }
     }
     res.sendStatus(200);
