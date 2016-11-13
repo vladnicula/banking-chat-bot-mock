c6da51cd65e2ae799860ff9b6b69849301072f99
Vlad Nicula <vladnicula@gmail.com>
2016-11-13 11:49:46 +0200

    Case insensitive names

--- fixtures/index.js
+++ fixtures/index.js
@@ -1,11 +1,11 @@
 const userService = require('../services/user-service');
 
 const USERS = [
-    {name: 'Raul', chatId: '981647388611508', balance: 1250.18, balanceSavings: 3000},
-    {name: 'Vlad', chatId: '1221584201246326', balance: 322.53, balanceSavings: 3000},
-    {name: 'Bogdan', chatId: '1325850250780262', balance: 320.00, balanceSavings: 3000},
-    {name: 'Horia', chatId: '1203276786414242', balance: 500, balanceSavings: 3000},
-    {name: 'Vivianne', chatId: '1130662566983525', balance: 755, balanceSavings: 3000}
+    {name: 'raul', chatId: '981647388611508', balance: 1250.18, balanceSavings: 3000},
+    {name: 'vlad', chatId: '1221584201246326', balance: 322.53, balanceSavings: 3000},
+    {name: 'bogdan', chatId: '1325850250780262', balance: 320.00, balanceSavings: 3000},
+    {name: 'horia', chatId: '1203276786414242', balance: 500, balanceSavings: 3000},
+    {name: 'vivianne', chatId: '1130662566983525', balance: 755, balanceSavings: 3000}
 ];
 
 const USER_IDS = USERS.map(userDetails => {
